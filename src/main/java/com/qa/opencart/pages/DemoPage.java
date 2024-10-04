acb10b1fcfe3648c28e4787a76cd649649a36192
shivdumka <shivam.dumka2@gmail.com>
2024-10-04 20:05:02 +0530

    created Demopage

---
+++ src/main/java/com/qa/opencart/pages/DemoPage.java
@@ -0,0 +1,17 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class DemoPage {
+
+	public static void main(String[] args) 
+	
+	{
+		By username=By.linkText("demo");
+		
+		System.out.println("please pass the username and password and you will get logged in");
+
+	}
+	
+	
+}
